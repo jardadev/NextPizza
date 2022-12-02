a878a0cfbea239d591fd0609c30ce1d1b5d02927
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-02 13:00:58 -0800

    refactor register form as standalone component

--- components/Forms/RegisterForm.jsx
+++ components/Forms/RegisterForm.jsx
@@ -1,41 +1,62 @@
-import FormContainer from './FormContainer';
+import { useState } from 'react';
 import { FaArrowCircleRight } from 'react-icons/fa';
 
 
-const RegisterForm = () => {
+const RegisterForm = ({handleRegistrationSubmit}) => {
+	
+	const [form, setForm] = useState({fullName: '', email: '', password: ''});
+	const [confirmPassword, setConfirmPassword] = useState('');
+
 	return (
-		<FormContainer>
-			<h1 class='mb-8 text-3xl text-center'>Sign up</h1>
-			<input
-				type='text'
-				class='block border border-grey-light w-full p-3 rounded mb-4'
-				name='fullname'
-				placeholder='Full Name'
-			/>
+		<form
+			className='bg-grey-lighter'
+			onSubmit={(e) => {
+				e.preventDefault()
+				handleRegistrationSubmit(form)
+			}}
+		>
+			<div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
+				<div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
+					<h1 className='mb-8 text-3xl text-center'>Sign up</h1>
+					<input
+						type='text'
+						className='block border border-grey-light w-full p-3 rounded mb-4'
+						placeholder='Full Name'
+						value={form.fullName}
+						onChange={(e) => setForm({...form, fullName: e.target.value})}
+					/>
 
-			<input
-				type='text'
-				class='block border border-grey-light w-full p-3 rounded mb-4'
-				name='email'
-				placeholder='Email'
-			/>
+					<input
+						type='text'
+						className='block border border-grey-light w-full p-3 rounded mb-4'
+						placeholder='Email'
+						value={form.email}
+						onChange={(e) => setForm({...form, email: e.target.value})}
+					/>
 
-			<input
-				type='password'
-				class='block border border-grey-light w-full p-3 rounded mb-4'
-				name='password'
-				placeholder='Password'
-			/>
-			<input
-				type='password'
-				class='block border border-grey-light w-full p-3 rounded mb-4'
-				name='confirm_password'
-				placeholder='Confirm Password'
-			/>
-			<button className='flex justify-center w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1'>
+					<input
+						type='password'
+						className='block border border-grey-light w-full p-3 rounded mb-4'
+						placeholder='Password'
+						value={form.password}
+						onChange={(e) => setForm({...form, password: e.target.value})}
+					/>
+					<input
+						type='password'
+						className='block border border-grey-light w-full p-3 rounded mb-4'
+						placeholder='Confirm Password'
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>
+					<button
+						type='submit'
+						className='flex justify-center w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1'
+					>
 						<FaArrowCircleRight size={24} />
 					</button>
-		</FormContainer>
+				</div>
+			</div>
+		</form>
 	);
 };
 
