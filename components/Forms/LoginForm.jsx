8bff6493f4c90cc2437929e7faa0e0f9051751f7
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-02 13:00:38 -0800

    refactor login form as a standalone component

--- components/Forms/LoginForm.jsx
+++ components/Forms/LoginForm.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import FormContainer from './FormContainer';
 import { FaArrowCircleRight } from 'react-icons/fa';
+
 const LoginForm = (props) => {
 	return (
-		<FormContainer>
-			<h1 class='mb-8 text-3xl text-center'>Log In</h1>
+		<form
+			className='bg-grey-lighter'
+			onSubmit={(e) => handleRegisterFormSubmission(e)}
+		>
+			<div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
+				<div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
+					<h1 className='mb-8 text-3xl text-center'>Log In</h1>
 
-			<input
-				type='text'
-				class='block border border-grey-light w-full p-3 rounded mb-4'
-				name='email'
-				placeholder='Email'
-			/>
+					<input
+						type='text'
+						className='block border border-grey-light w-full p-3 rounded mb-4'
+						name='email'
+						placeholder='Email'
+					/>
 
-			<input
-				type='password'
-				class='block border border-grey-light w-full p-3 rounded mb-4'
-				name='password'
-				placeholder='Password'
-			/>
+					<input
+						type='password'
+						className='block border border-grey-light w-full p-3 rounded mb-4'
+						name='password'
+						placeholder='Password'
+					/>
 
-			<button className='flex justify-center w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1'>
-				<FaArrowCircleRight size={24} />
-			</button>
-		</FormContainer>
+					<button className='flex justify-center w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1'>
+						<FaArrowCircleRight size={24} />
+					</button>
+				</div>
+			</div>
+		</form>
 	);
 };
 
