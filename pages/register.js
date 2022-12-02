95ca476f43e21b48b0bd14399dba4c15820b1ee5
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-02 13:02:12 -0800

    add submit button handler function

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,30 @@
 import RegisterForm from '../components/Forms/RegisterForm';
 import ConnectionsBtn from '../components/UI/ConnectionsBtn';
 const Register = () => {
+	const handleRegistrationSubmit = async (data) => {
+		try {
+			create(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// CREATE Customer
+	const create = async (data) => {
+		await fetch('/api/customer/create', {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json; charset=utf8',
+			},
+		})
+			.then(console.log('done'))
+			.catch((err) => console.log(err));
+	};
+	
 	return (
 		<div>
-			<RegisterForm />
+			<RegisterForm handleRegistrationSubmit={handleRegistrationSubmit} />
 			<div>
 				<div className='py-3'>
 					<p className='text-center'>OR</p>
