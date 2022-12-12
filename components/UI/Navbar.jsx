b9ff8252fc10b4d34cc5d4a615694681647b1b60
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-12 14:12:24 -0800

    refactor remove session conditional checks

--- components/UI/Navbar.jsx
+++ components/UI/Navbar.jsx
@@ -7,43 +7,26 @@ const Navbar = ({ children }) => {
 	const session = useSession();
 
 	return (
-		<div className='h-screen flex flex-col'>
-			<div className='flex justify-between flex-row items-center p-3'>
-				<>
-					<h1 className='text-2xl'>Pizza Time</h1>
-				</>
-				<>
-					{session.status === 'unauthenticated' &&
-						router.pathname === '/' && (
-							<Link
-								className='underline text-blue-500 '
-								href={'/register'}
-							>
-								Don&apos;t have an Account? Register.
-							</Link>
-						)}
-					{session.status === 'unauthenticated' &&
-						router.pathname === '/register' && (
-							<Link
-								className='underline text-blue-500 '
-								href={'/'}
-							>
-								Already have an Account? Sign in.
-							</Link>
-						)}
-					{session.status === 'authenticated' && <PageNav />}
-				</>
-			</div>
-			<div className='grid place-items-center p-8'>{children}</div>
-			<footer>
-				<br />
-				<span className='flex justify-center items-center text-sm underline p-1'>
-					<Link href={'https://github.com/jardadev'}>
-						Created by Aaron Johnson
+		<header className='w-full flex justify-between border-b border-grey p-4'>
+			<>
+				<h1 className='text-2xl'>Pizza Time</h1>
+			</>
+			<>
+				{router.pathname === '/' && (
+					<Link
+						className='underline text-blue-500 '
+						href={'/register'}
+					>
+						Don&apos;t have an Account? Register.
 					</Link>
-				</span>
-			</footer>
-		</div>
+				)}
+				{router.pathname === '/register' && (
+					<Link className='underline text-blue-500 ' href={'/'}>
+						Already have an Account? Sign in.
+					</Link>
+				)}
+			</>
+		</header>
 	);
 };
 
