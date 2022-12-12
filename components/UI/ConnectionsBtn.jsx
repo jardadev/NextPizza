b908efd97a6ca6e9f15424305c4416278ee99f20
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-12 14:11:19 -0800

    replace div with button element

--- components/UI/ConnectionsBtn.jsx
+++ components/UI/ConnectionsBtn.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { FaDiscord, FaGoogle, FaGithub } from 'react-icons/fa';
 import { signIn } from 'next-auth/react';
 
 const ConnectionsBtn = () => {
-  return (
-	<div
-					className='p-2 flex flex-col text-center cursor-pointer border rounded-2xl justify-center items-center bg-blue-200 hover:bg-blue-300'
-					onClick={() => signIn()}
-				>
-					<p>Register with:</p>
-					<ul className='flex justify-center'>
-						<div className='flex gap-1'>
-							<li>
-								<FaDiscord size={16} />
-							</li>
-							<li>
-								<FaGoogle size={16} />
-							</li>
-							<li>
-								<FaGithub size={16} />
-							</li>
-						</div>
-					</ul>
+	return (
+		<button
+			type='button'
+			className='btn p-2 text-center cursor-pointer border rounded-2xl bg-blue-200 hover:bg-blue-300'
+			onClick={() => signIn()}
+		>
+			<p>Login/Register with:</p>
+			<ul className='flex justify-center'>
+				<div className='flex gap-1'>
+					<li>
+						<FaDiscord size={16} />
+					</li>
+					<li>
+						<FaGoogle size={16} />
+					</li>
+					<li>
+						<FaGithub size={16} />
+					</li>
 				</div>
-  )
-}
+			</ul>
+		</button>
+	);
+};
 
-export default ConnectionsBtn
+export default ConnectionsBtn;
