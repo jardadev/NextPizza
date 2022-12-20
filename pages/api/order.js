f9f29ee0d1fdbee147eeaca01fa4700334a6a508
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-20 14:58:46 -0800

    add order model CRUD

---
+++ pages/api/order.js
@@ -0,0 +1,50 @@
+// pages/api/order
+
+import {
+	createOrder,
+	deleteOrder,
+	getAllOrders,
+	getOrder,
+	updateOrder,
+} from '../../lib/prisma'
+
+export default async function handle(req, res) {
+	try {
+		switch (req.method) {
+			case 'GET': {
+				if (req.query.id) {
+					// Get a single order if id is provided is the query
+					// api/orders?id=1
+					const order = await getOrder(req.query.id);
+					return res.status(200).json(order);
+				} else {
+					// Otherwise, fetch all orders
+					const orders = await getAllOrders();
+					return res.json(orders);
+				}
+			}
+			case 'POST': {
+				// Create a new order
+				const { email, name, birthYear } = req.body;
+				const order = await createOrder(email, name, birthYear);
+				return res.json(order);
+			}
+			case 'PUT': {
+				// Update an existing order
+				const { id, ...updateData } = req.body;
+				const order = await updateOrder(id, updateData);
+				return res.json(order);
+			}
+			case 'DELETE': {
+				// Delete an existing order
+				const { id } = req.body;
+				const order = await deleteOrder(id);
+				return res.json(order);
+			}
+			default:
+				break;
+		}
+	} catch (error) {
+		return res.status(500).json({ ...error, message: error.message });
+	}
+}
