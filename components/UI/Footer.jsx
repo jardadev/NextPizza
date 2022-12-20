8654ea3f38f5366a0502428429b98cef8bf93e3a
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-19 20:47:05 -0800

    add styles

--- components/UI/Footer.jsx
+++ components/UI/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Link from 'next/link';
 const Footer = () => {
 	return (
-		<footer>
+		<footer className='w-full h-16 bg-gray-600 border-t-2 border-white
+		fixed left-0 bottom-0
+		flex justify-center items-center
+		text-white text-2xl'>
 			<br />
 			<span className='flex justify-center items-center text-sm underline p-1'>
 				<Link href={'https://github.com/jardadev'}>
