2f5b9c6d974e5f5aabd5b034d985cdef2243f6c5
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-19 20:49:30 -0800

    refactor user profile image card component

--- components/UI/UserCard.jsx
+++ components/UI/UserCard.jsx
@@ -1,23 +1,16 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
-
-const UserCard = ({session}) => {
+const UserCard = ({ session }) => {
 	return (
-		<div>
+		<>
 			<Image
-				className='rounded-full w-8 mr-4'
-				src={session.data.user.image}
-				alt={session.data.user.name + "'s image"}
+				className='rounded-full'
+				src={session.data?.user.image}
+				alt={session.data?.user.name + "'s image"}
 				height={80}
 				width={80}
 			/>
-			<span className='text-gray-500 mr-4'>{session.data.user.name}</span>
-			<Link className='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900' href='/api/auth/signout'>
-					Sign Out
-				
-			</Link>
-		</div>
+		</>
 	);
 };
 
