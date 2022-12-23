3ae652efc6ce40d385dd844b1ba0e4e683218a89
AaronOldLinux <jardaaroncs@gmail.com>
2022-12-22 19:41:13 -0800

    add neworder page

---
+++ pages/orders/new.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NewPizzaForm from '../../components/Forms/NewPizzaForm';
+const newOrder = () => {
+	return (
+		<>
+			<NewPizzaForm />
+		</>
+	);
+};
+
+export default newOrder;
